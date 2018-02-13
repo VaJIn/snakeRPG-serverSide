4c9e566f4da328467f8baba20dbb931a29b5d632
jules.leguy <jules.leguy55@gmail.com>
2018-02-13 16:55:13 +0100

    DB première version

---
+++ database/src/test/dbsnake_SnakeClass.sql
@@ -0,0 +1,2 @@
+INSERT INTO dbsnake.SnakeClass (id, name) VALUES (1, 'default');
+INSERT INTO dbsnake.SnakeClass (id, name) VALUES (2, 'special');
---
+++ database/src/test/scriptDB.sql
@@ -0,0 +1,78 @@
+CREATE TABLE Game
+(
+  id         INT(10) AUTO_INCREMENT
+    PRIMARY KEY,
+  startTime  TIMESTAMP(19) DEFAULT CURRENT_TIMESTAMP     NOT NULL,
+  endTime    TIMESTAMP(19) DEFAULT '0000-00-00 00:00:00' NOT NULL,
+  idGameMode INT(10)                                     NOT NULL
+);
+
+CREATE TABLE GameMode
+(
+  id        INT(10) AUTO_INCREMENT
+    PRIMARY KEY,
+  name      VARCHAR(64)        NOT NULL,
+  minPlayer INT(10) DEFAULT 0  NOT NULL,
+  maxPlayer INT(10) DEFAULT 10 NOT NULL
+);
+
+CREATE UNIQUE INDEX GameMode_name_uindex
+  ON GameMode (name);
+
+ALTER TABLE Game
+  ADD CONSTRAINT Game_GameMode_id_fk
+FOREIGN KEY (idGameMode) REFERENCES GameMode (id);
+
+CREATE TABLE GameParticipation
+(
+  idGame  INT(10) NOT NULL,
+  idSnake INT(10) NOT NULL,
+  PRIMARY KEY (idGame, idSnake),
+  CONSTRAINT GameParticipation_Game_id_fk
+  FOREIGN KEY (idGame) REFERENCES Game (id)
+);
+
+CREATE TABLE Snake
+(
+  id           INT(10) AUTO_INCREMENT
+    PRIMARY KEY,
+  userID       INT(10)           NOT NULL,
+  name         VARCHAR(64)       NOT NULL,
+  exp          INT(10) DEFAULT 0 NOT NULL,
+  info         BLOB              NULL,
+  idSnakeClass INT(10)           NOT NULL
+);
+
+ALTER TABLE GameParticipation
+  ADD CONSTRAINT GameParticipation_Snake_id_fk
+FOREIGN KEY (idSnake) REFERENCES Snake (id);
+
+CREATE TABLE SnakeClass
+(
+  id   INT(10) AUTO_INCREMENT
+    PRIMARY KEY,
+  name VARCHAR(64) NOT NULL
+);
+
+ALTER TABLE Snake
+  ADD CONSTRAINT Snake_SnakeClass_id_fk
+FOREIGN KEY (idSnakeClass) REFERENCES SnakeClass (id);
+
+CREATE TABLE User
+(
+  id          INT(10) AUTO_INCREMENT
+    PRIMARY KEY,
+  alias       VARCHAR(64)  NOT NULL,
+  email       VARCHAR(128) NOT NULL,
+  accountName VARCHAR(64)  NOT NULL,
+  password    VARCHAR(256) NOT NULL
+);
+
+CREATE UNIQUE INDEX User_accountName_uindex
+  ON User (accountName);
+
+ALTER TABLE Snake
+  ADD CONSTRAINT snake_User_id_fk
+FOREIGN KEY (userID) REFERENCES User (id);
+
+
