4c94f1c139e780aebadfd9674671a68cbe6262f3
valentine <valentine.rahier@etud.univ-angers.fr>
2018-02-14 13:40:48 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ database/src/test/resources/dbsnake_GameMode.sql
@@ -0,0 +1,2 @@
+INSERT INTO GameMode (id, name, minPlayer, maxPlayer) VALUES (1, 'defaultGame', 2, 15);
+INSERT INTO GameMode (id, name, minPlayer, maxPlayer) VALUES (2, 'superSpecialOufGame', 50, 2000);
