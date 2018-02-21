ff7eaf54eb8e70ff7055dc42804e122d0413d017
Paulin Violette <violette.paulin@gmail.com>
2018-02-21 16:02:18 +0100

    Link GameEntity GameParticipation

--- database/src/main/java/fr/vajin/snakerpg/database/daoimpl/GameDAOImpl.java
+++ database/src/main/java/fr/vajin/snakerpg/database/daoimpl/GameDAOImpl.java
@@ -3,8 +3,10 @@ package fr.vajin.snakerpg.database.daoimpl;
 import fr.vajin.snakerpg.database.DAOFactory;
 import fr.vajin.snakerpg.database.GameDAO;
 import fr.vajin.snakerpg.database.GameModeDAO;
+import fr.vajin.snakerpg.database.GameParticipationDAO;
 import fr.vajin.snakerpg.database.entities.GameEntity;
 import fr.vajin.snakerpg.database.entities.GameModeEntity;
+import fr.vajin.snakerpg.database.entities.GameParticipationEntity;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -19,7 +21,10 @@ public class GameDAOImpl implements GameDAO {
     private static String db_adr = "jdbc:mysql://localhost:3306/dbsnake";
     private Statement statement;
 
-    public GameDAOImpl(){
+    private DAOFactory daoFactory;
+
+    public GameDAOImpl(DAOFactory daoFactory) {
+        this.daoFactory = daoFactory;
         Connection con = null;
         try {
             Properties connectionProp = new Properties();
@@ -57,7 +62,19 @@ public class GameDAOImpl implements GameDAO {
             out = null;
         }
 
-        return Optional.ofNullable(out);
+
+        if (out == null) {
+            return Optional.empty();
+        } else {
+
+            GameParticipationDAO gameParticipationDAO = this.daoFactory.getGameParticipationDAO();
+
+            List<GameParticipationEntity> gameParticipationEntityList = gameParticipationDAO.getGameResultsByGame(out.getId(), DAOFactory.SORT_BY_SCORE_ASC);
+
+            out.setParticipationEntitySet(new HashSet<>(gameParticipationEntityList));
+
+            return Optional.of(out);
+        }
     }
 
 
@@ -146,16 +163,16 @@ public class GameDAOImpl implements GameDAO {
 
     private GameEntity resultSetToGameEntity(ResultSet rs) throws SQLException {
 
-        int id = rs.getInt("id");
-        Timestamp startTime = rs.getTimestamp("startTime");
-        Timestamp endTime = rs.getTimestamp("endTime");
-        int idGameMode = rs.getInt("idGameMode");
+        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+0"));
 
+        int id = rs.getInt("id");
+        Timestamp startTime = rs.getTimestamp("startTime", calendar);
+        Timestamp endTime = rs.getTimestamp("endTime", calendar);
+        int idGameMode = rs.getInt("idGameMode");
 
         //Paulin 21/02 -> passage par la factory
 //        GameModeDAO gameModeDAO = new GameModeDAOImpl(); //Useless
-        DAOFactory factory = new DAOFactoryImpl();
-        GameModeDAO gameModeDAO = factory.getGameModeDAO();
+        GameModeDAO gameModeDAO = this.daoFactory.getGameModeDAO();
 
         //If it's not present there is a problem with the database
         GameModeEntity gameModeEntity = gameModeDAO.getGameMode(idGameMode).get();
