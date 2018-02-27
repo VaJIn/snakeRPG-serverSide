41105a3c9907bd671a1404e603ec5f1ab4fa816e
Paulin Violette <violette.paulin@gmail.com>
2018-02-27 10:10:21 +0100

    Paulin boulet (fix GameDAOImpl:getGameParticipationByIds)

--- database/src/main/java/fr/vajin/snakerpg/database/GameParticipationDAO.java
+++ database/src/main/java/fr/vajin/snakerpg/database/GameParticipationDAO.java
@@ -4,6 +4,7 @@ import fr.vajin.snakerpg.database.entities.GameParticipationEntity;
 
 import java.sql.Timestamp;
 import java.util.List;
+import java.util.Optional;
 
 public interface GameParticipationDAO {
 
@@ -12,7 +13,7 @@ public interface GameParticipationDAO {
     List<GameParticipationEntity> getGameResultsByGame(int gameId, int sortBy, boolean retrieveGameEntity);
     List<GameParticipationEntity> getGameResultsByGame(int gameId, int sortBy);
 
-    List<GameParticipationEntity> getGameParticipationByIds(int gameId, int snakeId, int sortBy);
+    Optional<GameParticipationEntity> getGameParticipationByIds(int gameId, int snakeId, int sortBy);
 
     List<GameParticipationEntity> getGameParticipation(Timestamp earliest, Timestamp lastest, int sortBy);
 }
